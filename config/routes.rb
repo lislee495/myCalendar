b6286bf220721d4b75a0932c1b362cb144a1b19b
Lisa Lee <snickers495@gmail.com>
2017-12-18 15:23:22 -0600

    added bootstrap to the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+    resources :events
+    resources :friendships
+  end
+
+  root :to => 'application#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
