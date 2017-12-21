5d92ac940f96e19e283b15069a60d8337d14ee74
Lisa Lee <snickers495@gmail.com>
2017-12-21 17:57:07 -0600

    fleshed out friendship feature

--- app/controllers/friendship_controller.rb
+++ app/controllers/friendship_controller.rb
@@ -1,21 +1,41 @@
 class User::FriendshipController < ApplicationController
   def new
+    @friendship = current_user.friendships.build
   end
 
   def create
-  end
-
-  def edit
-  end
-
-  def update
+    if find_friend
+      @friendship = current_user.friendships.build(friend_id: user.id)
+      if @friendship.save
+        flash[:notice] = "Added friend."
+        redirect_to root_url
+      else
+        flash[:notice] = "Unable to add friend."
+        redirect_to root_url
+      end
+    else
+      flash[:notice] = "Unable to find friend."
+      redirect_to root_url
+    end
   end
 
   def destroy
+    @friendship.find_by(user_id: current_user.id, friend_id: User.find_by(username: friend_params[:username]))
+    @friendship.delete
+    redirect_to root_url
   end
 
   private
   def friend_params
     params.require(:user).permit(:email, :username)
+  end
+
+  def find_friend
+    if friend_params[:username]
+      user = User.find_by(username: friend_param[:username])
+    else
+      user = User.find_by(username: friend_param[:email])
+    end
+    user
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
     resources :events
     resources :friendships
   end
-  get "/user/month", to: "events#month"
-  get "/user/week", to: "events#week"
-  get "/user/today", to: "events#today"
+  get "/user/month", to: "user/events#month"
+  get "/user/week", to: "user/events#week"
+  get "/user/today", to: "user/events#today"
   root :to => 'application#welcome'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
