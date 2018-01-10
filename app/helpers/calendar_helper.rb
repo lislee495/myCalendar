d7963227baa284ac0199b778f0298fbb7c9a2a4d
Lisa Lee <snickers495@gmail.com>
2018-01-10 12:22:02 -0600

    made proto Day Calendar

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -7,5 +7,7 @@ module CalendarHelper
     Calendar.new(self, date, block).week_table
   end
 
-
+  def day_calendar(date = Date.today, &block)
+    Calendar.new(self, date, block).week_table
+  end
 end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -3,16 +3,10 @@ class Calendar < Struct.new(:view, :date, :callback)
     START_DAY = :sunday
 
     delegate :content_tag, to: :view
-
+    ###############################MONTH######################################
     def table
       content_tag :table, class: "calendar table table-bordered table-striped" do
         header + week_rows
-      end
-    end
-
-    def week_table
-      content_tag :table, class: "calendar table table-bordered table-striped" do
-        header + day_rows
       end
     end
 
@@ -30,20 +24,9 @@ class Calendar < Struct.new(:view, :date, :callback)
       end.join.html_safe
     end
 
-    def day_rows
-      content_tag :tr do
-        week.map { |day| content_tag :td, view.capture(day, &callback), class: week_day_classes(day) }.join.html_safe
-      end
-    end
-
     def day_cell(day)
       content_tag :td, view.capture(day, &callback), class: day_classes(day)
     end
-
-    def week_day_cell(day)
-      content_tag :td, view.capture(day, &callback), class: week_day_classes(day)
-    end
-
 
     def day_classes(day)
       classes = []
@@ -52,17 +35,34 @@ class Calendar < Struct.new(:view, :date, :callback)
       classes.empty? ? nil : classes.join(" ")
     end
 
+    def weeks
+      first = date.beginning_of_month.beginning_of_week(START_DAY)
+      last = date.end_of_month.end_of_week(START_DAY)
+      (first..last).to_a.in_groups_of(7)
+    end
+    ########################WEEK############################
+
+    def week_table
+      content_tag :table, class: "calendar table table-bordered table-striped" do
+        header + day_rows
+      end
+    end
+
+    def day_rows
+      content_tag :tr do
+        week.map { |day| content_tag :td, view.capture(day, &callback), class: week_day_classes(day) }.join.html_safe
+      end
+    end
+
+    def week_day_cell(day)
+      content_tag :td, view.capture(day, &callback), class: week_day_classes(day)
+    end
+
     def week_day_classes(day)
       classes = []
       classes << "today" if day == Date.today
       classes << "not-week" if week_day(day) != week_day(date)
       classes.empty? ? nil : classes.join(" ")
-    end
-
-    def weeks
-      first = date.beginning_of_month.beginning_of_week(START_DAY)
-      last = date.end_of_month.end_of_week(START_DAY)
-      (first..last).to_a.in_groups_of(7)
     end
 
     def week
