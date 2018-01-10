761f8a53fd4cd2204d1270742f1a455b6ecf3529
Lisa Lee <snickers495@gmail.com>
2018-01-10 09:25:15 -0600

    finished the week calendar

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -2,4 +2,10 @@ module CalendarHelper
   def calendar(date = Date.today, &block)
     Calendar.new(self, date, block).table
   end
+
+  def week_calendar(date = Date.today, &block)
+    Calendar.new(self, date, block).week_table
+  end
+
+
 end
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,5 +1,5 @@
 class Calendar < Struct.new(:view, :date, :callback)
-    HEADER = %w[Sunday Monday Tuesday Wednesday Thursday Friday Saturday]
+    HEADER = %w[Sun Mon Tue Wed Thur Fri Sat]
     START_DAY = :sunday
 
     delegate :content_tag, to: :view
@@ -7,6 +7,12 @@ class Calendar < Struct.new(:view, :date, :callback)
     def table
       content_tag :table, class: "calendar table table-bordered table-striped" do
         header + week_rows
+      end
+    end
+
+    def week_table
+      content_tag :table, class: "calendar table table-bordered table-striped" do
+        header + day_rows
       end
     end
 
@@ -24,9 +30,20 @@ class Calendar < Struct.new(:view, :date, :callback)
       end.join.html_safe
     end
 
+    def day_rows
+      content_tag :tr do
+        week.map { |day| content_tag :td, view.capture(day, &callback), class: week_day_classes(day) }.join.html_safe
+      end
+    end
+
     def day_cell(day)
       content_tag :td, view.capture(day, &callback), class: day_classes(day)
     end
+
+    def week_day_cell(day)
+      content_tag :td, view.capture(day, &callback), class: week_day_classes(day)
+    end
+
 
     def day_classes(day)
       classes = []
@@ -35,9 +52,28 @@ class Calendar < Struct.new(:view, :date, :callback)
       classes.empty? ? nil : classes.join(" ")
     end
 
+    def week_day_classes(day)
+      classes = []
+      classes << "today" if day == Date.today
+      classes << "not-week" if week_day(day) != week_day(date)
+      classes.empty? ? nil : classes.join(" ")
+    end
+
     def weeks
       first = date.beginning_of_month.beginning_of_week(START_DAY)
       last = date.end_of_month.end_of_week(START_DAY)
+      (first..last).to_a.in_groups_of(7)
+    end
+
+    def week
+      first = date.beginning_of_week(START_DAY)
+      last = date.end_of_week(START_DAY)
+      (first..last).to_a
+    end
+
+    def week_day(day)
+      first = date.beginning_of_week(START_DAY)
+      last = date.end_of_week(START_DAY)
       (first..last).to_a.in_groups_of(7)
     end
 end
