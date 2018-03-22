0b439eb29537ca7673e3c84d5441262627cd9af3
Lisa Lee <lisa_lee@alumni.brown.edu>
2018-03-22 18:02:18 -0500

    need event info from categories

--- app/controllers/user/category_controller.rb
+++ app/controllers/user/category_controller.rb
@@ -2,10 +2,18 @@ class User::CategoryController < ApplicationController
   before_action :authenticate_user!
   helper CategoryHelper
   def index
-    @categories = current_user.categories.uniq if current_user.categories
+    @categories = current_user.categories.uniq 
+    respond_to do |format|
+      format.html { render :index }
+      format.json { render json: @categories }
+    end
   end
 
   def show
     @category = Category.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @category }
+    end
   end
 end
